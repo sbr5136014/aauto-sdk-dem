a7431de12ecc4aacb6177f95e71dff5facb5559c
sbr5136014 <sbr5136014@gmail.com>
2022-07-18 21:58:57 -0400

    update

---
+++ app/src/main/java/clRoot/OnClickListener.java
@@ -0,0 +1,11 @@
+package clRoot;//package ;
+
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.martoreto.aademo.wifiSettingsActivity;
+import com.google.android.apps.auto.sdk.CarToast;
+
+
+    // do your stuff
+
